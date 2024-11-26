975b54208daa78e7ddd71f11b3a20546cca6cdf2
nlvdoanh <doanhnguyen.31231020575@st.ueh.edu.vn>
2024-11-26 18:19:43 +0700

    Add files via upload

---
+++ Bai tap Data types.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+
+namespace Bai_tap_Data_types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            //Bai_01();
+            //Bai_02();
+            Bai_03();
+            Console.ReadKey();
+        }
+        public static void Bai_01()
+        {
+            Console.Write("Nhập nhiệt độ Celsius: ");
+            int celsius = int.Parse(Console.ReadLine());
+
+            int kelvin = celsius + 273;
+            Console.WriteLine("Nhiệt độ Kelvin là: " + kelvin);
+
+            int fahrenheit = celsius * 18 / 10 + 32;
+            Console.WriteLine("Nhiệt độ Fahrenheit là: " + fahrenheit);
+        }
+        public static void Bai_02()
+        {
+            Console.Write("Nhập bán kính hình cầu: ");
+            double r = double.Parse(Console.ReadLine());
+            double S = 4 * Math.Pow(r, 2) * Math.PI;
+            Console.WriteLine("Diện tích hình cầu là: " + S);
+            double V = 4 / 3 * Math.Pow(r, 3) * Math.PI;
+            Console.WriteLine("Thể tích hình cầu là: " + V);
+        }
+        public static void Bai_03()
+        {
+            Console.Write("Nhập số a: ");
+            int a = int.Parse(Console.ReadLine());
+          
+            Console.Write("Nhập số b: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("a + b = " + (a + b));
+            //Console.WriteLine($"a + b = {a + b} ");
+            Console.WriteLine("a - b = " + (a - b));
+            Console.WriteLine("a * b = " + (a * b));
+            Console.WriteLine("a / b = " + (a / b));
+            Console.WriteLine("Số dư của phép chia a / b = " + (a % b));
+        }
+    }
+}
+
---
+++ Bai tap Functions.cs
@@ -0,0 +1,81 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Bai_tap_Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            //Bai_01();
+            //Bai_02();
+            //Bai_03();
+            Bai_04();
+            Console.ReadKey();
+        }
+        public static void Bai_01()
+        {
+            Console.Write("Nhập vào số thứ 1: ");
+            int st1 = int.Parse(Console.ReadLine());
+            Console.Write("Nhập vào số thứ 2: ");
+            int st2 = int.Parse(Console.ReadLine());
+            Console.Write("Nhập vào số thứ 3: ");
+            int st3 = int.Parse(Console.ReadLine());
+
+            int max = st1;
+            if (st2 > max)
+            {
+                if (st3 > max)
+                {
+                    Console.WriteLine($"Số lớn nhất là {st3}");
+                }
+                else
+                    Console.WriteLine($"Số lớn nhất là {st2}");
+            }
+            else
+                Console.WriteLine($"Số lớn nhất là {st1}");
+            Console.ReadKey();
+        }
+        public static void Bai_02()
+        {
+            Console.Write("Nhập vào một số: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = a;
+            int giaithua = 1;
+            while (a > 0)
+            {
+                giaithua *= a;
+                a--;
+            }
+            Console.WriteLine($"Giai thừa của {b} = {giaithua}");
+        }
+        public static void Bai_03()
+        {
+            Console.Write("Nhập vào một số: ");
+            int a = int.Parse(Console.ReadLine());
+            int count = 0;
+            for (int i = 1; i <= a; i++)
+                if (a % i == 0) count++;
+            {
+                if (count > 2)
+                {
+                    Console.WriteLine("Đây không phải là số nguyên tố");
+                }
+                else
+                    Console.WriteLine("Đây là số nguyên tố");
+            }
+        }
+        public static void Bai_04()
+        {
+            Console.Write("Nhập 1 số: ");
+            int n = int.Parse(Console.ReadLine());
+            int S = 0;
+            for (int i = 1; i < n; i++) 
+            if (n % i == 0) S = S + i;
+            Console.WriteLine($"Tổng các ước của {n} = {S}");
+            if (S == n) Console.WriteLine($"{n} là số hoàn hảo.");
+            else Console.WriteLine($"{n} không là số hoàn hảo.");
+        }
+    }
+}
